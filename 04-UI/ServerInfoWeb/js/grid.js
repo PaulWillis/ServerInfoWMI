343913c3aa209c97b24ca65293b80c29e37ff616
Paul Willis <PaulWillis@users.noreply.github.com>
2015-09-30 15:20:57 -0400

    remove comments

--- 04-UI/ServerInfoWeb/js/grid.js
+++ 04-UI/ServerInfoWeb/js/grid.js
@@ -50,8 +50,6 @@
             rowData: null // set rowData to null or undefined to show loading panel by default
         };
 
-        //http://10.0.6.147/serverinfowebservice/api/serverinfoset
-        //"http://localhost:60667/api/serverinfoset"
         $http.get("http://localhost:60667/api/serverinfoset")
             .then(function (res) {
                 $scope.gridOptions.rowData = res.data;
@@ -68,40 +66,5 @@
 
 
          
-    //var module = angular.module("example", ["angularGrid"]);
-
-    //module.controller("exampleCtrl", function($scope, $http) {
-
-    //    var columnDefs = [
-    //        {headerName: "Athlete", field: "athlete", width: 150},
-    //        {headerName: "Age", field: "age", width: 90},
-    //        {headerName: "Country", field: "country", width: 120},
-    //        {headerName: "Year", field: "year", width: 90},
-    //        {headerName: "Date", field: "date", width: 110},
-    //        {headerName: "Sport", field: "sport", width: 110},
-    //        {headerName: "Gold", field: "gold", width: 100},
-    //        {headerName: "Silver", field: "silver", width: 100},
-    //        {headerName: "Bronze", field: "bronze", width: 100},
-    //        {headerName: "Total", field: "total", width: 100}
-    //    ];
-
-    //    $scope.gridOptions = {
-    //        columnDefs: columnDefs,
-    //        rowData: null
-    //    };
-
-    //    $http.get("http://localhost:60667/api/serverinfoset")
-    //        .then(function(res){
-    //            $scope.gridOptions.rowData = res.data;
-    //            $scope.gridOptions.api.onNewRows();
-    //        });
-    //});
-
-
-        
-
-
-
-
 
 }());
